103e92c9afbb8e23fa26305fade46451320d244d
wuyuexiang <wuyuexiang129@163.com>
2022-10-25 14:03:51 +0800

    代码优化

--- coupon-template-serv/coupon-template-repo/src/main/java/com/geekbang/coupon/repository/entity/CouponTemplate.java
+++ coupon-template-serv/coupon-template-repo/src/main/java/com/geekbang/coupon/repository/entity/CouponTemplate.java
@@ -30,37 +30,39 @@ import java.time.LocalDateTime;
 @Table(name = "coupon_template")
 public class CouponTemplate implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+	private static final long serialVersionUID = -8259173057496361376L;
 
-    // 状态是否可用
-    @Column(name = "available", nullable = false)
-    private Boolean available;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+	// 状态是否可用
+	@Column(name = "available", nullable = false)
+	private Boolean available;
 
-    // 适用门店-如果为空，则为全店满减券
-    @Column(name = "shop_id")
-    private Long shopId;
+	@Column(name = "name", nullable = false)
+	private String name;
 
-    @Column(name = "description", nullable = false)
-    private String description;
+	// 适用门店-如果为空，则为全店满减券
+	@Column(name = "shop_id")
+	private Long shopId;
 
-    // 优惠券类型
-    @Column(name = "type", nullable = false)
-    @Convert(converter = CouponTypeConverter.class)
-    private CouponType type;
+	@Column(name = "description", nullable = false)
+	private String description;
 
-    // 创建时间，通过@CreateDate注解自动填值（需要配合@JpaAuditing注解在启动类上生效）
-    @CreatedDate
-    @Column(name = "created_time", nullable = false)
-    private LocalDateTime createdTime;
+	// 优惠券类型
+	@Column(name = "type", nullable = false)
+	@Convert(converter = CouponTypeConverter.class)
+	private CouponType type;
 
-    // 优惠券核算规则，平铺成JSON字段
-    @Column(name = "rule", nullable = false)
-    @Convert(converter = RuleConverter.class)
-    private TemplateRule rule;
+	// 创建时间，通过@CreateDate注解自动填值（需要配合@JpaAuditing注解在启动类上生效）
+	@CreatedDate
+	@Column(name = "created_time", nullable = false)
+	private LocalDateTime createdTime;
+
+	// 优惠券核算规则，平铺成JSON字段
+	@Column(name = "rule", nullable = false)
+	@Convert(converter = RuleConverter.class)
+	private TemplateRule rule;
 }
